016da0d0d8c5f7a5923b0027ce6299ba52c5a0be
Andrew Klotz <agc.klotz@gmail.com>
2018-12-09 23:55:53 +0000

    dockerize

--- app/backend-repo/main.go
+++ app/backend-repo/main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
-	"k99/app/lib/tracing"
 	"log"
 	"net/http"
 
 	opentracing "github.com/opentracing/opentracing-go"
+
+	"k99/app/lib/client"
+	"k99/app/lib/tracing"
 )
+
+func pingHandler() func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		_, span := client.ContextFromHTTP("pinging", r)
+		defer span.Finish()
+
+		w.Write([]byte("pong"))
+	}
+}
 
 func main() {
 	tracer, closer := tracing.New("backend-repo")
--- app/backend/main.go
+++ app/backend/main.go
@@ -6,8 +6,19 @@ import (
 
 	opentracing "github.com/opentracing/opentracing-go"
 
+	"k99/app/lib/client"
 	"k99/app/lib/tracing"
 )
+
+func pingHandler() func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx, span := client.ContextFromHTTP("pinging", r)
+		defer span.Finish()
+
+		client.PingService(ctx, "http://0.0.0.0:3002/ping", "ping-backend-repo")
+		w.Write([]byte("pong"))
+	}
+}
 
 func main() {
 	tracer, closer := tracing.New("backend")
